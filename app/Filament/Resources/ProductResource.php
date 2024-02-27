3ea9c3403a07b9aefccddfb77624d3ba1b0f8aa7
Nikoloz Lomtadze <lomtadzenikusha@gmail.com>
2024-02-27 23:51:58 +0400

    feat: add tags relation manager

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -74,7 +74,7 @@ class ProductResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            RelationManagers\TagsRelationManager::class,
         ];
     }
 
--- database/migrations/2024_02_27_191810_create_tags_table.php
+++ database/migrations/2024_02_27_191810_create_tags_table.php
@@ -10,17 +10,9 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('tags', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->integer('price');
-            $table->enum(
-                'status',
-                ['in stock', 'sold out', 'coming soon']
-            )
-                ->default('in stock');
-            $table->foreignId('category_id')->nullable()->constrained();
-            $table->foreignId('tag_id')->nullable()->constrained();
             $table->timestamps();
         });
     }
@@ -30,6 +22,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('tags');
     }
 };
